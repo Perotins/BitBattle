bf655500c40741aaef68bdc241b57496be71d2be
Abstraction <n3xuz@stamina.pw>
2017-11-06 03:47:34 +0100

    Added simple BitPlayer and Trait interface

---
+++ src/main/java/me/perotin/bitbattle/BitPlayer.java
@@ -0,0 +1,17 @@
+package me.perotin.bitbattle;
+
+import me.perotin.bitbattle.traits.Trait;
+
+import java.util.Optional;
+
+public interface BitPlayer {
+
+    double getHealth();
+
+    //TODO: Returns true if the player died
+    boolean damage(double amount);
+
+    void apply(Trait trait);
+
+    <T extends Trait> Optional<T> getTrait(Class<T> traitType);
+}
---
+++ src/main/java/me/perotin/bitbattle/traits/Trait.java
@@ -0,0 +1,5 @@
+package me.perotin.bitbattle.traits;
+
+public interface Trait {
+
+}

dab3836bc451346ea2173d3f0689d98463025ccb
Perotin <maxfuligni@gmail.com>
2017-11-05 20:21:00 -0500

    First commit, created main class and weapons class

---
+++ src/main/java/me/perotin/bitbattle/BitBattle.java
@@ -0,0 +1,29 @@
+package me.perotin.bitbattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+    Plugin started on 11/05/17
+
+    For committing to the repository, make sure to include a descriptive message. It will then be
+    reviewed and either approved or denied appropriately.
+
+    For documentation on the plugin you can visit this link
+    https://docs.google.com/document/d/1okG7dZr_JQ91rvUvxSz_-XvidK2h0oqYD2WTv89Tsug/edit?usp=sharing
+
+    If you want to contribute on a deeper level, and be an integral part of the project,
+    contact Perotin on discord (Perotin#3447)
+
+    All dating in comments and throughout the repository are in month / day / year format.
+
+ */
+
+public class BitBattle extends JavaPlugin{
+
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}

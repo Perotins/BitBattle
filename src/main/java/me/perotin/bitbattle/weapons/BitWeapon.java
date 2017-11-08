e8eb07cc07707bb24bae31c64478356073b1ab71
Perotin <maxfuligni@gmail.com>
2017-11-07 21:17:14 -0500

    Created ProjectileWeapon and MeleeWeapon classes

--- src/main/java/me/perotin/bitbattle/weapons/BitWeapon.java
+++ src/main/java/me/perotin/bitbattle/weapons/BitWeapon.java
@@ -1,17 +1,14 @@
 package me.perotin.bitbattle.weapons;
 
-import java.util.HashMap;
 
 /*
     Created 11/05/17 by Perotin
 
 
 
-    Class used to create, load, and manage weapons
-    Weapons fields change as apart of upgrades, none of the fields will be constant
-    besides for the name and projectile during runtime
+     class used for handling projectile weapons and melee weapons
  */
-public class BitWeapon {
+public  class BitWeapon {
     /*
     TODO
         Expand on this class as we find more integral parts that we can convert from Arrow.io
@@ -19,67 +16,29 @@ public class BitWeapon {
     */
 
     // name of the weapon
-    private String name;
-
+    private final String name;
     /*
-        @percentage
-        percentage of victim's max health,
-
-        percentages should be created out of 100
-        Example: player has 10 hearts, weapon has a damage percentage of 10%, player loses 1 heart
-
-      */
-    private int damagePercentage;
-
-    /*
-        @returns a boolean if the weapon is thrown, if false, weapon is melee
+        @int
+        base value, don't make higher than 2
 
      */
-    private boolean projectile;
+    private final int baseDamage;
+
 
     /*
-        Amount of projectiles shot at the same time
-        for melee weapons this will be 0
-     */
-    private int amountShotAtOnce;
-
-    /*
-        Amount of projectiles shot in procession,
-        for melee weapons this will be 0
-     */
-    private int amountShotInARow;
-
-    /*
-        stores the weapon itself as the key, and the ammunition as the value
-        for melee weapons set the value as AIR
-        Strings stored should be a Material#toString, saving as strings for parsing purposes
-     */
-    private HashMap<String, String> weaponBuild;
-
-    /*
-        Cooldown of which a player can fire/attack again, this can be
-        set to 0 in the config for all weapons
+    @cooldown until the weapon can be used again
      */
     private int cooldown;
 
 
     // default weapon constructor, pull from config so they can customize the defaults
-    public BitWeapon(String name, int damagePercentage, boolean projectile, int amountShotAtOnce, int amountShotInARow, HashMap<String, String> weaponBuild, int cooldown) {
-        this.name = name;
-        this.damagePercentage = 20;
-        this.projectile = projectile;
-        // defaults for projectile will be just one shot at once, none shot in a row
-        if(projectile) {
-            this.amountShotAtOnce = 1;
-            this.amountShotInARow = 0;
-        } else {
-            // setting to zero because its melee
-            this.amountShotAtOnce = 0;
-            this.amountShotInARow = 0;
-        }
-        this.weaponBuild = weaponBuild;
-        this.cooldown = 3;
-    }
+   public BitWeapon(String name, int baseDamage, int cooldown){
+       this.name = name;
+       this.baseDamage = baseDamage;
+       this.cooldown = cooldown;
+
+   }
+
 
 
     public String getName() {
@@ -88,43 +47,10 @@ public class BitWeapon {
 
 
 
-    public int getDamagePercentage() {
-        return damagePercentage;
+    public int getBaseDamage() {
+        return baseDamage;
     }
 
-    public void setDamagePercentage(int damagePercentage) {
-        this.damagePercentage = damagePercentage;
-    }
-
-
-
-    public void setProjectile(boolean projectile) {
-        this.projectile = projectile;
-    }
-
-    public int getAmountShotAtOnce() {
-        return amountShotAtOnce;
-    }
-
-    public void setAmountShotAtOnce(int amountShotAtOnce) {
-        this.amountShotAtOnce = amountShotAtOnce;
-    }
-
-    public int getAmountShotInARow() {
-        return amountShotInARow;
-    }
-
-    public void setAmountShotInARow(int amountShotInARow) {
-        this.amountShotInARow = amountShotInARow;
-    }
-
-    public HashMap<String, String> getWeaponBuild() {
-        return weaponBuild;
-    }
-
-    public void setWeaponBuild(HashMap<String, String> weaponBuild) {
-        this.weaponBuild = weaponBuild;
-    }
 
     public int getCooldown() {
         return cooldown;
---
+++ src/main/java/me/perotin/bitbattle/weapons/subweapons/MeleeWeapon.java
@@ -0,0 +1,24 @@
+package me.perotin.bitbattle.weapons.subweapons;
+
+import me.perotin.bitbattle.weapons.BitWeapon;
+
+/*
+    Created 11/07/17 by Perotin
+
+    Class for melee weapons
+ */
+public class MeleeWeapon extends BitWeapon {
+
+    /*
+    TODO
+    think of cool stuff we can make here
+     */
+    public MeleeWeapon(String name, int baseDamage, int cooldown){
+        super(name, baseDamage, cooldown);
+    }
+
+
+
+
+
+}
